f422d860068c9c03f80e2ab9c374c99b25d45098
Lukas Gumm <lukas@die-gumms.de>
2020-11-07 12:22:00 +0100

    implemented LogIn Page (Not MainWindows anymore) Prepared extra Window for Students to show data only Prepared MainWindow to show and edit/change data in future Commit.
    Next Step: Creat Dummy SQL-Database and Connection to implement first data for ListBox-Items.
    
    No code clean-up and commenting done. ;)

---
+++ UI MockUp/LogInPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace UI_MockUp
+{
+    /// <summary>
+    /// Interaktionslogik für LogInPage.xaml
+    /// </summary>
+    public partial class LogInPage : Window
+    {
+        public LogInPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnClickLogInAsStudent(object sender, RoutedEventArgs e)
+        {
+            ViewerForStudents viewerForStudents = new ViewerForStudents();
+            viewerForStudents.Show();
+            this.Close();
+            //später würde ich die LoginPage erst schließen, wenn die Anmeldung erfolgreich war. Das Mainwindow würde ich benutzen, um Seiten für die versch. Sichten zu realisieren. Im Moment werden dafür einzelne Windows benutzt.
+        }
+
+        private void OnClickLogInAsLecturer(object sender, RoutedEventArgs e)
+        {
+            if (UsernameBox.Text== "Test" && PasswordBox.Password == "Test")
+            {
+                Application.Current.MainWindow.Visibility = Visibility.Visible;
+            }
+        }
+    }
+}
--- UI MockUp/ViewerForStudents.xaml.cs
+++ UI MockUp/ViewerForStudents.xaml.cs
@@ -10,29 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace UI_MockUp
 {
     /// <summary>
-    /// Interaktionslogik für MainWindow.xaml
+    /// Interaktionslogik für ViewerForStudents.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class ViewerForStudents : Window
     {
-        public MainWindow()
+        public ViewerForStudents()
         {
             InitializeComponent();
-            OpenLogIn();
         }
-
-        public void OpenLogIn()
-        {
-            LogInPage logInPage = new LogInPage();
-            logInPage.Show();
-            logInPage.Focus();
-            this.Visibility = Visibility.Hidden;             
-        }
-                
     }
 }
